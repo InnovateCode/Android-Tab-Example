85c41235817b974fe564cd6f54944030a06f1354
dev_a <devcontactus@gmail.com>
2015-11-18 21:53:17 +0800

    Implement FragmentPagerAdapter class

--- app/src/main/java/innovatecode/com/tabexample/MainActivity.java
+++ app/src/main/java/innovatecode/com/tabexample/MainActivity.java
@@ -1,6 +1,9 @@
 package innovatecode.com.tabexample;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -15,4 +18,40 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
     }
+
+    public static class TabsExamplePagerAdapter extends FragmentPagerAdapter {
+        // As we are implementing two tabs
+        private static final int NUM_ITEMS = 2;
+
+        public TabsExamplePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        // For each tab different fragment is returned
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new TabOneFragment();
+                case 1:
+                    return new TabTwoFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // For simplicity of this tutorial this string is hardcoded
+            // Otherwise it should be access using string resource
+            return "Tab " + (position + 1);
+        }
+    }
+
 }
